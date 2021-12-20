38d4a278393c529a28208b4a3d47d044d0f77b68
Bruce Parrello <bparrello@figresearch.com>
2021-12-20 09:12:35 -0600

    First branch

---
+++ src/main/java/org/theseed/rna/erdb/App.java
@@ -0,0 +1,58 @@
+package org.theseed.rna.erdb;
+
+import java.util.Arrays;
+
+import org.theseed.utils.BaseProcessor;
+
+/**
+ * Commands for utilities relating to RNA-Seq processing.
+ *
+ * fpkm			run jobs to convert FASTQ files to FPKM results
+ * metaLoad		load genome metadata into an SQL database
+ * download		download a set of RNA results from PATRIC and produce a metadata file for them
+ * upload		process RNA results from PATRIC and add them to a database
+ * measureLoad	load measurements into an SQL database
+ * sampleCorr	output the correlations between a genome's samples in the RNA database
+ * clusterLoad	refresh the cluster IDs from a sample correlation table
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        // Get the control parameter.
+        String command = args[0];
+        String[] newArgs = Arrays.copyOfRange(args, 1, args.length);
+        BaseProcessor processor;
+        // Determine the command to process.
+        switch (command) {
+        case "fpkm" :
+            processor = new RnaSeqProcessor();
+            break;
+        case "metaLoad" :
+            processor = new MetaLoadProcessor();
+            break;
+        case "measureLoad" :
+            processor = new MeasureLoadProcessor();
+            break;
+        case "download" :
+            processor = new SampleDownloadProcessor();
+            break;
+        case "upload" :
+            processor = new SampleUploadProcessor();
+            break;
+        case "sampleCorr" :
+            processor = new SampleCorrelationProcessor();
+            break;
+        case "clusterLoad" :
+            processor = new ClusterLoadProcessor();
+            break;
+        default:
+            throw new RuntimeException("Invalid command " + command);
+        }
+        // Process it.
+        boolean ok = processor.parseCommand(newArgs);
+        if (ok) {
+            processor.run();
+        }
+    }
+}
